7881facb3c5bce6e105659822a6d2566243d16cf
Codushan <118370072+Codushan@users.noreply.github.com>
2024-12-10 21:36:12 +0530

    Update Signup.js

--- frontend/src/Pages/Signup.js
+++ frontend/src/Pages/Signup.js
@@ -30,7 +30,7 @@ const Signup = () => {
             return handleError('All fields are required');
         }
         try {
-            const URL = 'https://test-backend-production-8542.up.railway.app/auth/signup';
+            const URL = 'https://test-backend-production-8542.up.railway.app/signup';
             const response = await fetch(URL, {
                 method: 'POST',
                 headers: {
