5e07444ef3c809fa1360b92958d6e199f82d321e
Codushan <118370072+Codushan@users.noreply.github.com>
2024-12-11 02:24:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 
 // const router = express.Router();
 
-const PORT = 4444 || process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 
