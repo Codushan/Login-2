9b09c2c64016f6c25d911ece380e12ce9cd80eb2
Codushan <118370072+Codushan@users.noreply.github.com>
2025-02-16 02:40:24 +0530

    Update Login.js
    v

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -32,7 +32,7 @@ const handleLogin = async (e) => {
         return handleError('email and password are required');
     }
     try {
-        const URL = 'https://test-backend-production-8542.up.railway.app/login';
+        const URL = 'https://login-2-api.vercel.app/login';
         const response = await fetch(URL, {
             method: 'POST',
             headers: {
