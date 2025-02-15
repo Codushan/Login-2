558b2bfcc98d277cb65bb63749413ac63e678c12
Codushan <118370072+Codushan@users.noreply.github.com>
2025-02-16 02:49:46 +0530

    Update Signup.js

--- frontend/src/Pages/Signup.js
+++ frontend/src/Pages/Signup.js
@@ -30,7 +30,7 @@ const Signup = () => {
             return handleError('All fields are required');
         }
         try {
-            const URL = 'https://login-2-api.vercel.app/Auth/signup';
+            const URL = 'https://login-2-api.vercel.app/auth/signup';
             const response = await fetch(URL, {
                 method: 'POST',
                 headers: {
