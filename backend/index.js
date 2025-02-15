9fd1e008d77afaeabdbb210828a99e56f66127d3
Codushan <118370072+Codushan@users.noreply.github.com>
2025-02-16 02:29:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,28 +7,25 @@ const authRouter = require('./routes/AuthRouter');
 const productRouter = require('./routes/ProductRouter');
 const cors = require('cors');
 
-// const router = express.Router();
-
-const PORT = process.env.PORT || 8080;
+const PORT = 4444 || process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.json('Hello');
 });
-app.use(bodyParser.json());
-app.use(cors(
-    {
-        origin:true,
-        methods:["POST","GET"],
-        credentials:true
-    }
-));
+
+app.use(cors({
+    origin: true,
+    methods: ["POST", "GET"],
+    credentials: true
+}));
 
 app.use(express.json());
+
 app.use('/auth', authRouter);
 app.use('/products', productRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
